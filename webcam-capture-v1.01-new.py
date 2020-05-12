4e403a4c0d0ae54bf9c8c9a91efff24af064253e
Collin Brake <collin.brake@ventrac.com>
2020-05-12 13:55:42 -0400

    import opencv2 as cv instead of cv2

--- webcam-capture-v1.01-new.py
+++ webcam-capture-v1.01-new.py
@@ -1,22 +1,22 @@
-import cv2
+import cv2 as cv
 
-key = cv2.waitKey(1)
-webcam = cv2.VideoCapture(0)
+key = cv.waitKey(1)
+webcam = cv.VideoCapture(0)
 while True:
 
     check, frame = webcam.read()
     print(check) #prints true as long as the webcam is running
     print(frame) #prints matrix values of each framecd
-    cv2.imshow("Capturing", frame)
-    key = cv2.waitKey(1)
+    cv.imshow("Capturing", frame)
+    key = cv.waitKey(1)
     if key == ord('s'):
-        cv2.imwrite(filename='saved_img.jpg', img=frame)
+        cv.imwrite(filename='saved_img.jpg', img=frame)
         webcam.release()
         img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-        cv2.imshow("Captured Image", img_new)
-        cv2.waitKey(1650)
-        cv2.destroyAllWindows()
-        cv2.imwrite(filename='saved_img-final.jpg', img=img_new)
+        cv.imshow("Captured Image", img_new)
+        cv.waitKey(1650)
+        cv.destroyAllWindows()
+        cv.imwrite(filename='saved_img-final.jpg', img=img_new)
         print("Saved BGR image as grayscale")
 
         break
@@ -25,5 +25,5 @@ while True:
         webcam.release()
         print("Camera off.")
         print("Program ended.")
-        cv2.destroyAllWindows()
+        cv.destroyAllWindows()
         break
